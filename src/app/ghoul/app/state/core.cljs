2118371e3d9fad0a55100a536377b4e4bd6126f2
alonso.torres <alonso.torres@kaleidos.net>
2015-09-10 21:02:52 +0200

    Second definition of the new state

--- src/app/ghoul/app/state/core.cljs
+++ src/app/ghoul/app/state/core.cljs
@@ -6,9 +6,8 @@
   []
   {:display-mode [:list :expanded-view]
    :selected [:all-items]
-   :groups []
-   :feeds []
-   :items []})
+   :index []
+   :feeds {}})
 
 (defn get-display-mode
   "Retrieves the selected display-mode in the application"
@@ -20,35 +19,43 @@
   [state]
   (:selected state))
 
-(defn get-groups
-  "Retrieves a list of the groups currently in the state"
+(defn list-index
+  "Retrieve the current defined index. This is the elements the user have available
+  in her menu"
   [state]
-  (:groups state))
+  (:index state))
 
-(defn get-feeds
+(defn list-feeds
   "Retrieves the list of feeds in the state"
-  [state]
-  (:feeds state))
-
-(defn get-items
-  "Retrieves the items in the current state. Depends on the current value of `get-selected`"
-  [state]
-  (:items state))
-
-(defn get-feed-by-uid
-  "Retrieves a feed by a given uid"
-  [state uid]
-  (first (filter #(= (:uid %) uid) (get-feeds state))))
+  ([state]
+   (vals (:feeds state)))
+  ([state & {:keys [group]}]
+   (filter #(= (:group %) group) (vals (:feeds state)))))
 
 ;; State transitions
-(defmulti state-transition
+(defmulti update-state
   "Definition for state transitions. Payloads are vectors with it's first
    value the transition to be defined."
   (fn [_ [transition _]] transition))
 
-(defmethod state-transition :add-feed [state [_ feed-data]]
+(defmethod update-state :add-feed [state [_ feed-data]]
   "Adds a new feed to the list of feeds. Will fail if the feed uid existed or the data doesn't
    match the schema for feed data"
   ;; TODO validate feed-data schema
   ;; TODO make sure it's an unique uid
-  (update-in state [:feeds] conj feed-data))
+  (-> state
+      (assoc-in [:feeds (:uid feed-data)] feed-data)
+      (update-in [:index] conj {:feed (:uid feed-data)})))
+
+(defmethod update-state :add-group [state [_ group-name]]
+  "Creates a new empty group inside the state. A group is only defined by its name"
+  (update-in state [:index] conj {:group group-name}))
+
+(defmethod update-state :set-group-feed [state [_ feed-uid group-name]]
+  "Sets the feed group to a new value"
+  ;; TODO Fail if feed-uid is not in the feed list
+  (-> state
+      ;; If the feed was in the index we remove it
+      (assoc-in [:index] (into [] (filter #(not (= (:feed %) feed-uid)) (:index state))))
+      (assoc-in [:feeds feed-uid :group] group-name)))
+
--- src/test/ghoul/app/state/core_test.cljs
+++ src/test/ghoul/app/state/core_test.cljs
@@ -7,26 +7,50 @@
     (let [state (state/empty-state)]
       (is (= [:list :expanded-view] (state/get-display-mode state)))
       (is (= [:all-items] (state/get-selected state)))
-      (is (empty? (state/get-groups state)))
-      (is (empty? (state/get-feeds state)))
-      (is (empty? (state/get-items state)))))
+      (is (empty? (state/list-index state)))
+      (is (empty? (state/list-feeds state)))))
 
   (testing "Adds a feed into the state"
     (let [feed-uid  "truly-unique-1"
           feed-data {:title "Osom feed title" :link "http://rss.com/osom.rss" :uid feed-uid}
           state     (-> (state/empty-state)
-                        (state/state-transition [:add-feed feed-data]))]
-      (is (= [:list :expanded-view] (state/get-display-mode state)))
-      (is (= [:all-items] (state/get-selected state)))
-      (is (= (first (state/get-feeds state)) feed-data))
-      (is (= (state/get-feed-by-uid state feed-uid)))
-      (is (empty? (state/get-groups state)))
-      (is (empty? (state/get-items state))))))
+                        (state/update-state [:add-feed feed-data]))]
+      (is (= (first (state/list-feeds state)) feed-data))
+      (is (= 1 (count (state/list-index state))))
+      (is (= {:feed feed-uid} (first (state/list-index state))))))
+
+  (testing "Create an empty group"
+    (let [group-name  "Group"
+          state     (-> (state/empty-state)
+                        (state/update-state [:add-group group-name]))]
+      (is (= (first (state/list-index state)) {:group group-name}))
+      (is (empty? (state/list-feeds state :group group-name)))
+      (is (= 1 (count (state/list-index state))))
+      (is (= {:group group-name} (first (state/list-index state))))))
+
+  (testing "Add feed into a group"
+    (let [group1 "Group 1"
+          feed1  {:title "Feed 1" :link "http://rss.com/feed1.rss" :uid "feed1"}
+          feed2  {:title "Feed 2" :link "http://rss.com/feed2.rss" :uid "feed2"}
+          transitions [[:add-feed feed1]
+                       [:add-feed feed2]
+                       [:add-group group1]
+                       [:set-group-feed "feed1" group1]]
+          state (reduce state/update-state (state/empty-state) transitions)]
+      (is (= 2 (count (state/list-feeds state))))
+      (is (= 1 (count (state/list-feeds state :group group1 ))))
+      (is (= (assoc feed1 :group group1) (first (state/list-feeds state :group group1))))
+
+      (is (= 2 (count (state/list-index state))))
+      (is (= {:feed "feed2"}  (first (state/list-index state))))
+      (is (= {:group group1} (second (state/list-index state)))))))
 
 #_(deftest state-transition-invalid-transitions
   (testing "Try to add an invalid feed")
   (testing "Try to add an invalid item")
+  (testing "Try to add a duplicated group")
   (testing "Try to add a feed with the same uid as an existing one")
   (testing "Try to add an item when there is no feed")
-  (testing "Try to move a feed to an unexisting group"))
+  (testing "Try to move a feed to an unexisting group")
+  (testing "Try to move a non-existing feed to a group"))
 
