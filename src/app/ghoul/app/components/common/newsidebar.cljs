59964fd5459f1b18f1477a992a7d25378abf39fe
alonso.torres <alonso.torres@kaleidos.net>
2015-10-02 23:47:16 +0200

    Separated components for sidebar

--- src/app/ghoul/app/components/common/newsidebar.cljs
+++ src/app/ghoul/app/components/common/newsidebar.cljs
@@ -2,7 +2,8 @@
   (:require [om.core :as om]
             [om-tools.core :refer-macros [defcomponent]]
             [om-tools.dom :as dom :include-macros true]
-            [ghoul.app.messages :refer [msg]]))
+            [ghoul.app.messages :refer [msg]]
+            [ghoul.common.utils :refer [active-classes]]))
 
 (defcomponent filters [data owner]
   (render [this]
@@ -12,49 +13,52 @@
         (dom/li (dom/a {:className "favorites" :href "#"} "Favorites"))
         (dom/li (dom/a {:className "shared" :href "#"} "Shared"))))))
 
+(defcomponent subscription [data owner]
+  (render [this]
+    (let [favicon     (or (-> data :feed :favicon) "/images/rssicon.png")
+          title       (-> data :feed :title)
+          count       (or (-> data :feed :pending) 0)
+          is-selected false]
+      (dom/li {:className (active-classes {:selected is-selected})}
+        (dom/img {:className "favicon" :src favicon})
+        (dom/a {:href "#"} title (dom/span {:className "count"} (str count)))))))
+
+(defcomponent group [data owner]
+  (render [this]
+    (let [make-feed-data
+          (fn [feed]
+            (-> {}
+                (assoc :selected (:selected data))
+                (assoc :feed feed)))
+
+          group-name (-> data :group :name)
+          group-selected (= (-> data :selected second) group-name)
+          group-feed-uid-set (-> data :group :subscriptions set)
+          group-feeds (filter #(-> % :uid group-feed-uid-set) (:feeds data))]
+
+      (dom/li {:className (active-classes ["group"] {:selected group-selected})}
+        (dom/a {:className "toggle-view" :href "#"} "expand")
+        (dom/a {:className "group-title" :href "#"}
+               (dom/span {:className "group-title-text"} group-name)
+               (dom/span {:className "count"} "2"))
+        (dom/ul
+          (om/build-all subscription (map make-feed-data group-feeds)))))))
+
 (defcomponent subscriptions [data owner]
   (render [this]
-    (dom/section {:className "subscriptions"}
+    (let [make-group-data
+          (fn [group]
+            (-> {}
+                (assoc :selected (:selected data))
+                (assoc :feeds (:feeds data))
+                (assoc :group group)))]
+
+      (dom/section {:className "subscriptions"}
         (dom/h2 "Susbcriptions")
         (dom/a {:className "add-group" :href "#"} "Add group")
 
         (dom/ul
-          (dom/li
-            (dom/img {:className "favicon" :src "/images/rssicon.png"})
-            (dom/a {:href "#"} "Taiga" (dom/span {:className "count"} "2")))
-          (dom/li
-            (dom/img {:className "favicon" :src "https://taiga.io/static/images/favicon.png"})
-            (dom/a {:href "#"} "Taiga" (dom/span {:className "count"} "2")))
-          (dom/li
-            (dom/img {:className "favicon" :src "https://taiga.io/static/images/favicon.png"})
-            (dom/a {:href "#"} "Taiga" (dom/span {:className "count"} "2")))
-          (dom/li {:className "group"}
-            (dom/a {:className "toggle-view" :href "#"} "expand")
-            (dom/a {:className "group-title" :href "#"}
-                   (dom/span {:className "group-title-text"} "Programming")
-                   (dom/span {:className "count"} "2"))
-            (dom/ul
-              (dom/li
-                (dom/img {:className "favicon" :src "https://www.redditstatic.com/favicon.ico"})
-                (dom/a {:href "#"} "Reddit programming" (dom/span {:className "count"} "2")))
-              (dom/li
-                (dom/img {:className "favicon" :src "http://slashdot.org/favicon.ico"})
-                (dom/a {:href "#"} "Slashdot" (dom/span {:className "count"} "2")))
-              ))
-          (dom/li {:className "group"}
-            (dom/a {:className "toggle-view" :href "#"} "expand")
-            (dom/a {:className "group-title" :href "#"}
-                   (dom/span {:className "group-title-text"} "Programming")
-                   (dom/span {:className "count"} "2"))
-            (dom/ul
-              (dom/li
-                (dom/img {:className "favicon" :src "https://www.redditstatic.com/favicon.ico"})
-                (dom/a {:href "#"} "Reddit programming" (dom/span {:className "count"} "2")))
-              (dom/li
-                (dom/img {:className "favicon" :src "http://slashdot.org/favicon.ico"})
-                (dom/a {:href "#"} "Slashdot" (dom/span {:className "count"} "2222")))
-              ))
-          ))))
+          (om/build-all group (map make-group-data (:groups data))))))))
 
 (defcomponent config [data owner]
   (render [this]
